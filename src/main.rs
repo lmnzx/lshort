da1a6f0636ca51ca12ed18d0647be0778ea96525
Sayan Mallick <snyxmk@gmail.com>
2022-11-08 12:38:49 +0530

    saving new urls to redis and generating short url

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 use {
     axum::{
         body::Bytes,
-        http::Request,
-        response::{Html, Response},
-        routing::get,
+        extract::{Extension, Json},
+        http::{Request, StatusCode},
+        middleware::{self},
+        response::{IntoResponse, Response},
+        routing::{get, post},
         Router,
     },
-    std::{net::SocketAddr, time::Duration},
+    lshort::metrics::{setup_metrics_recorder, track_metrics},
+    rand::distributions::{Alphanumeric, DistString},
+    redis::{Client, Commands},
+    std::sync::Arc,
+    std::{future::ready, net::SocketAddr, time::Duration},
     tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer},
     tracing::Span,
     tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt},
 };
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+#[derive(serde::Deserialize)]
+struct Link {
+    value: String,
+}
+
+async fn handler() -> impl IntoResponse {
+    (StatusCode::OK, "OK")
+}
+
+async fn new_link(
+    Extension(client): Extension<Arc<Client>>,
+    Json(link): Json<Link>,
+) -> impl IntoResponse {
+    let mut con = client.get_connection().expect("Failed to connect to redis");
+    println!("{}", link.value);
+    let s = Alphanumeric.sample_string(&mut rand::thread_rng(), 6);
+    let _save_to_redis: () = con.set(link.value, &s).expect("Failed to set key");
+    (StatusCode::OK, format!("localhost:3000/r/{}", s))
+}
+
+pub async fn global_404() -> impl IntoResponse {
+    let message = "
+     __      __                              __ __       __   __ __      
+   /'__`\\  /'__`\\                           /\\ \\\\ \\    /'__`\\/\\ \\\\ \\     
+  /\\ \\/\\ \\/\\ \\/\\ \\  _____     ____          \\ \\ \\\\ \\  /\\ \\/\\ \\ \\ \\\\ \\    
+  \\ \\ \\ \\ \\ \\ \\ \\ \\/\\ '__`\\  /',__\\          \\ \\ \\\\ \\_\\ \\ \\ \\ \\ \\ \\\\ \\_  
+   \\ \\ \\_\\ \\ \\ \\_\\ \\ \\ \\L\\ \\/\\__, `\\__  __  __\\ \\__ ,__\\ \\ \\_\\ \\ \\__ ,__\\
+    \\ \\____/\\ \\____/\\ \\ ,__/\\/\\____/\\_\\/\\_\\/\\_\\\\/_/\\_\\_/\\ \\____/\\/_/\\_\\_/
+     \\/___/  \\/___/  \\ \\ \\/  \\/___/\\/_/\\/_/\\/_/   \\/_/   \\/___/    \\/_/  
+                      \\ \\_\\                                              
+                       \\/_/                                              
+  ";
+    (StatusCode::NOT_FOUND, message)
 }
 
 #[tokio::main]
@@ -25,13 +62,25 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let recorder_handle = setup_metrics_recorder();
+
+    let redis_client =
+        redis::Client::open("redis://127.0.0.1:6379").expect("Failed to connect to redis");
+
+    let redis_client = Arc::new(redis_client);
+
     let app = Router::new()
         .route("/", get(handler))
+        .route("/new", post(new_link))
+        .layer(Extension(redis_client))
+        .fallback(global_404)
         .layer(TraceLayer::new_for_http())
+        .route("/metrics", get(move || ready(recorder_handle.render())))
+        .route_layer(middleware::from_fn(track_metrics))
         .layer(
             TraceLayer::new_for_http()
                 .on_request(|request: &Request<_>, _span: &Span| {
-                    tracing::debug!("st`arted {} {}", request.method(), request.uri().path())
+                    tracing::debug!("started {} {}", request.method(), request.uri().path())
                 })
                 .on_response(|_response: &Response, latency: Duration, _span: &Span| {
                     tracing::debug!("response generated in {:?}", latency)
